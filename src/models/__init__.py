4814f77b6aef497f8e62cabcc17e59ddc8a68a8a
Walid MCHARA <60917211+walidmchara@users.noreply.github.com>
2026-08-15 19:49:32 +0300

    Update __init__.py

---
+++ src/models/__init__.py
@@ -0,0 +1,19 @@
+from .lstm import LSTMRegressor
+from .transformer import TransformerRegressor
+from .bilstm import BiLSTMRegressor
+from .gru import GRURegressor
+from .cnn_lstm import CNNLSTMRegressor
+from .cnn_gru import CNNGRURegressor
+from .cnn_bilstm_attention import CNNBiLSTMAttentionRegressor
+from .cnn_bigru_attention import CNNBiGRUAttentionRegressor
+
+__all__ = [
+    "LSTMRegressor",
+    "TransformerRegressor",
+    "BiLSTMRegressor",
+    "GRURegressor",
+    "CNNLSTMRegressor",
+    "CNNGRURegressor",
+    "CNNBiLSTMAttentionRegressor",
+    "CNNBiGRUAttentionRegressor",
+]
